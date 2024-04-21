98672cd1027ca0b8c641aa6f08e5d906d4c6132f
anil babu <anilbabu3245@gmail.com>
2024-04-22 00:59:46 +0530

    dd

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/.well-known/assetlinks.json",
-        destination: "/api/assetlinks",
-      },
-    ];
-  },
+ 
   images: {
     unoptimized: true,
   },

1af0fe3d4932c422d9a306766393065565e43b99
anil babu <anilbabu3245@gmail.com>
2024-04-22 00:58:51 +0530

    dd

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/.well-known/assetlinks.json",
+        destination: "/api/assetlinks",
+      },
+    ];
+  },
+  images: {
+    unoptimized: true,
+  },
+  // rules: {
+  //   "@next/next/no-img-element": "off",
+  // },
+  output: "export",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
+
+
+
